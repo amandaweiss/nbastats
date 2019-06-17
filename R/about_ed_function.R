9e5e6cff520938c947a435f52d988b9c09c73dc5
amandaweiss <51291089+amandaweiss@users.noreply.github.com>
2019-06-17 09:40:57 -0400

    Update about_ed_function.R

--- R/about_ed_function.R
+++ R/about_ed_function.R
@@ -1,3 +1,13 @@
+#' Ed Bartels' mean true shooting percentage
+#'
+#' This function returns Ed Bartels' mean true shooting percentage
+#' @param yearinput
+#' @keywords Ed Bartels,TS
+#' @export
+#' @examples
+#' about_ed(1952)
+
+
 about_ed <- function(x){
 
   ed_ts <- mean((dplyr::filter(nba_data, Year==x, Player=="Ed Bartels"))$`TS%`)
